a9513a0a3d06e1611e5aa4adaf187ac1b38d1677
ah3q <akira.hasegawa@riken.jp>
2019-07-26 13:42:21 +0900

    update on 20190726.134221

--- workflow/larvae/revision/dataprocess.20190723.sh
+++ workflow/larvae/revision/dataprocess.20190723.sh
@@ -1,16 +1,23 @@
 if [ $# -gt 0 ] ; then
 nodeid=`perl rdf.pl -d larvae.sqlite3 newnode`
+perl rdf.pl -d larvae.sqlite3 insert larvae '#run' $nodeid
 perl rdf.pl -d larvae.sqlite3 insert $nodeid '#read1' $1
 perl rdf.pl -d larvae.sqlite3 insert $nodeid '#read2' $2
-perl rdf.pl -d larvae.sqlite3 insert root '#starindex' $3
-#perl rdf.pl -d larvae.sqlite3 software STAR samtools bedtools
+perl rdf.pl -d larvae.sqlite3 insert larvae '#host' $3
+perl rdf.pl -d larvae.sqlite3 insert larvae '#pathogen' $4
 #perl rdf.pl -d larvae.sqlite3 install STAR samtools bedtools
 fi
 
 perl moirai2.pl \
 -d larvae.sqlite3 \
--i '$id->#read1->$input1,$id->#read2->$input2' \
--o '$id->#bam->$bam,$id->#log->$log' \
+-i '$id->#read1->$input1,$id->#read2->$input2,larvae->#host->$starindex' \
+-o '$id->#hostbam->$bam,$id->#hostlog->$log' \
+https://moirai2.github.io/command/STAR/align_paired.json
+
+perl moirai2.pl \
+-d larvae.sqlite3 \
+-i '$id->#read1->$input1,$id->#read2->$input2,larvae->#pathogen->$starindex' \
+-o '$id->#pathogenbam->$bam,$id->#pathogenlog->$log' \
 https://moirai2.github.io/command/STAR/align_paired.json
 
 perl moirai2.pl \
@@ -27,24 +34,48 @@ https://moirai2.github.io/command/fastq/countseq.json
 
 perl moirai2.pl \
 -d larvae.sqlite3 \
--i '$id->#bam->$bam' \
+-i '$id->#hostbam->$bam' \
 -o '$id->#html->$html' \
 https://moirai2.github.io/command/sam/samstats.json
 
 perl moirai2.pl \
 -d larvae.sqlite3 \
--i '$id->#bam->$input' \
--o '$id->#multicount->$multicount,$id->#unmap1->$unmap1,$id->#unmap2->$unmap2' \
+-i '$id->#hostbam->$input' \
+-o '$id->#hostmulticount->$multicount,$id->#hostunmap1->$unmap1,$id->#hostunmap2->$unmap2' \
 https://moirai2.github.io/command/STAR/remove_multimap_paired.json
 
 perl moirai2.pl \
 -d larvae.sqlite3 \
--i '$id->#bam->$input' \
--o '$id->#sorted->$output' \
+-i '$id->#hostbam->$input' \
+-o '$id->#hostsorted->$output' \
 https://moirai2.github.io/command/samtools/sortbam.json
 
 perl moirai2.pl \
 -d larvae.sqlite3 \
--i '$id->#sorted->$input' \
--o '$id->#bed->$output' \
+-i '$id->#hostsorted->$input' \
+-o '$id->#hostbed->$output' \
+https://moirai2.github.io/command/bedtools/bam2bed.json
+
+perl moirai2.pl \
+-d larvae.sqlite3 \
+-i '$id->#pathogenbam->$bam' \
+-o '$id->#html->$html' \
+https://moirai2.github.io/command/sam/samstats.json
+
+perl moirai2.pl \
+-d larvae.sqlite3 \
+-i '$id->#pathogenbam->$input' \
+-o '$id->#pathogenmulticount->$multicount,$id->#pathogenunmap1->$unmap1,$id->#pathogenunmap2->$unmap2' \
+https://moirai2.github.io/command/STAR/remove_multimap_paired.json
+
+perl moirai2.pl \
+-d larvae.sqlite3 \
+-i '$id->#pathogenbam->$input' \
+-o '$id->#pathogensorted->$output' \
+https://moirai2.github.io/command/samtools/sortbam.json
+
+perl moirai2.pl \
+-d larvae.sqlite3 \
+-i '$id->#pathogensorted->$input' \
+-o '$id->#pathogenbed->$output' \
 https://moirai2.github.io/command/bedtools/bam2bed.json
